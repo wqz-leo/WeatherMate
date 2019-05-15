2b6c467eeed54be4c36afb53d6a072d2fe217601
leo.wang <wangqingzhouleo@gmail.com>
2019-05-15 22:34:19 +1000

    relocate files

--- src/WeatherMate/View/ViewController.swift
+++ src/WeatherMate/View/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
-        let request = WMAPI.Weather.GetCurrentWeather.Request(id: "2172797")
-        APIClient.default.makeRequest(request) { response in
-            print(response)
-        }
+//        let request = WMAPI.Weather.GetCurrentWeather.Request(id: "2172797")
+//        APIClient.default.makeRequest(request) { response in
+//            print(response)
+//        }
     }
 }
